12430dd9d0688da8cc4de954089a6d376ceb9604
laukho <lkhouri92@gmail.com>
2025-05-15 15:10:14 +0200

    login screen complete

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,10 +10,68 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // état du formulaire
+  // état de validité du formulaire
+  final _formKey = GlobalKey<FormState>();
+
+  // conserver la saisie du formulaire
+  final Map<String, String?> _type = {'email': null, 'password': null};
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Form(
+        // associer la clé de validité au formulaire
+        key: _formKey,
+        child: Column(
+          spacing: 20,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                icon: Icon(Icons.email),
+              ),
+              // contraintes de validation
+              // value représente la saisie
+              validator: (String? value) {
+                return value!.isEmpty ? 'Email is incorrect' : null;
+              },
+              // conserver la saisie
+              // value représente la saisie
+              onChanged: (value) {
+                _type['email'] = value;
+              },
+            ),
+            TextFormField(
+              // cacher la saisie
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                icon: Icon(Icons.password),
+              ),
+              // contraintes de validation
+              validator: (String? value) {
+                return value!.isEmpty ? 'Password is incorrect' : null;
+              },
+              onChanged: (value) {
+                _type['password'] = value;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // si le formulaire est valide
+                if (_formKey.currentState!.validate()) {
+                  // sauvegarder la saisie
+                  _formKey.currentState!.save();
+
+                  inspect(_type);
+                }
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
